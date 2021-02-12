42fd4ff18fe85ed62dad4b200c2439e51e076fab
FredFu <fjw08199999@gmail.com>
2021-02-13 07:22:58 +0800

    1.設定畫面完成 2.AutoLayOut完成 3.畫面與Main.StoryBoard連結完成

--- SpeakAPP_V3/ViewController.swift
+++ SpeakAPP_V3/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var setLangungeSegmented: UISegmentedControl!
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBOutlet weak var speedSliderValue: UISlider!
+    @IBOutlet weak var pitchMultiplierSliderValue: UISlider!
+    @IBOutlet weak var volumeSliderValue: UISlider!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var pitchMultiplierLabel: UILabel!
+    @IBOutlet weak var volumeLabel: UILabel!
+    @IBOutlet weak var speedLabelValue: UILabel!
+    @IBOutlet weak var pitchMultiplierLabelValue: UILabel!
+    @IBOutlet weak var volumeLabelValue: UILabel!
+    
+    @IBOutlet weak var randomButtenText: UIButton!
+    @IBOutlet weak var speakButtenText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func randomButten(_ sender: Any) {
+    }
+    
+    @IBAction func speakButten(_ sender: Any) {
+    }
 }
 
