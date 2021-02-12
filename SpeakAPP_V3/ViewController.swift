fdc76c3e1628d87c3391781e0bdb994ead30c8f9
FredFu <fjw08199999@gmail.com>
2021-02-13 06:58:07 +0800

    Initial Commit

---
+++ SpeakAPP_V3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeakAPP_V3
+//
+//  Created by fred fu on 2021/2/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
